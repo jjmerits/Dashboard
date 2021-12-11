aeea25e82f148b62ff0ffd04a047ac2deb04ddcd
jjmerits <35485369+jjmerits@users.noreply.github.com>
2021-12-11 18:48:38 +0900

    Update econdata.py

--- econdata.py
+++ econdata.py
@@ -282,7 +282,7 @@ with col1:
   df['url'] = df['url'].apply(make_clickable)
   #df.reset_index(drop=True, inplace=True)
   df = df.to_html(escape=False)
-  st.markdown(hide_table_row_index, unsafe_allow_html=True)
+  #st.markdown(hide_table_row_index, unsafe_allow_html=True)
   st.dataframe(df)
   #st.write(df, unsafe_allow_html=True)
   
