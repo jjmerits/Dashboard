ad5da987dd6c8176ea859e5ee4c6376529ad99dc
jjmerits <35485369+jjmerits@users.noreply.github.com>
2021-12-11 19:10:54 +0900

    Update econdata.py

--- econdata.py
+++ econdata.py
@@ -274,9 +274,9 @@ with col1:
   df['published date'] = df['published date'].apply(lambda x: datetime.strptime(x, '%a, %d %b %Y %H:%M:%S %Z').replace(tzinfo=timezone.utc))
   df['published date'] = df['published date'].dt.tz_convert('US/Eastern')
   df['published date'] = df['published date'].apply(lambda x: x.strftime('%d/%m/%y %H:%M:%S'))
-  df.rename(columns={'published date': 'date'}, inplace = True)
+  df.rename(columns={'published date': 'date (EST)'}, inplace = True)
     
-  #df.sort_values('date', inplace = True, ascending = False)
+  #df.sort_values('date (EST)', inplace = True, ascending = False)
   df.drop(['description','publisher'], axis=1, inplace = True)
   
   # link is the column with hyperlinks
@@ -299,9 +299,9 @@ with col2:
   df['published date'] = df['published date'].apply(lambda x: datetime.strptime(x, '%a, %d %b %Y %H:%M:%S %Z').replace(tzinfo=timezone.utc))
   df['published date'] = df['published date'].dt.tz_convert('US/Eastern')
   df['published date'] = df['published date'].apply(lambda x: x.strftime('%d/%m/%y %H:%M:%S'))
-  df.rename(columns={'published date': 'date'}, inplace = True)
+  df.rename(columns={'published date': 'date (EST)'}, inplace = True)
   
-  df.sort_values('date', inplace = True, ascending = False)
+  df.sort_values('date (EST)', inplace = True, ascending = False)
   df.drop(['description','publisher'], axis=1, inplace = True)
   
   # link is the column with hyperlinks
