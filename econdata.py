521aae65fe2412e4354378317cd19297a6e6fc5f
jjmerits <35485369+jjmerits@users.noreply.github.com>
2021-12-11 18:27:22 +0900

    Update econdata.py

--- econdata.py
+++ econdata.py
@@ -280,7 +280,7 @@ with col1:
   
   # link is the column with hyperlinks
   df['url'] = df['url'].apply(make_clickable)
-  df.reset_index(drop=True, inplace=True)
+  #df.reset_index(drop=True, inplace=True)
   df = df.to_html(escape=False)
   
   st.write(df, unsafe_allow_html=True)
@@ -305,7 +305,7 @@ with col2:
   
   # link is the column with hyperlinks
   df['url'] = df['url'].apply(make_clickable)
-  df.reset_index(drop=True, inplace=True)
+  #df.reset_index(drop=True, inplace=True)
   df = df.to_html(escape=False)
   
   st.write(df, unsafe_allow_html=True)
