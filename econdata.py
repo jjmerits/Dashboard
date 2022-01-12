f84757406dd5b8a91fd2636051f4d380242481fb
jjmerits <35485369+jjmerits@users.noreply.github.com>
2022-01-13 07:39:42 +0900

    Update econdata.py

--- econdata.py
+++ econdata.py
@@ -276,10 +276,12 @@ def gnews_html(q_str, cn='US', la='english'):
   df = df.reset_index().rename({'index':'importance'}, axis = 'columns')
   df['published date'] = df['published date'].apply(lambda x: datetime.strptime(x, '%a, %d %b %Y %H:%M:%S %Z').replace(tzinfo=timezone.utc))
   df['published date'] = df['published date'].dt.tz_convert('US/Eastern')
-  df['published date'] = df['published date'].apply(lambda x: x.strftime('%d/%m/%y %H:%M:%S'))
-  df.rename(columns={'published date': 'date (EST)'}, inplace = True)
+  #df['published date'] = df['published date'].apply(lambda x: x.strftime('%d/%m/%y %H:%M:%S'))
+  df['published date'] = datetime.today().astimezone(seo).astimezone(est) - df['published date']
+  df['published date'] = df['published date'].apply(lambda x: int(x.total_seconds()//3600))
+  df.rename(columns={'published date': 'hour ago'}, inplace = True)
     
-  df.sort_values('date (EST)', inplace = True, ascending = False)
+  df.sort_values('hour ago', inplace = True, ascending = False)
   df.drop(['description','publisher'], axis=1, inplace = True)
   # link is the column with hyperlinks
   df['url'] = df['url'].apply(make_clickable)
